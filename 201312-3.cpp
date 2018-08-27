e70ad0dc954f9c99e80d20bb64cda74218650ec8
Charlie <975447340@qq.com>
2018-08-27 10:45:54 +0800

    solve it

--- 201312-3.cpp
+++ 201312-3.cpp
@@ -1,48 +1,29 @@
 #include<iostream>
+
 using namespace std;
-
-int h[10000];
-int n;
-
-void a(int i,int max,int v)
-{
-	while(i<n-1)
-	{
-		if(h[i]<=h[i+1])
-		{
-			v=h[i]*2;
-			max=v;
-			a(i+1,max,v);
-		}
-		else
-		{
-			if(h[i+1]*2>=v)
-			{
-				v=h[i+1]*2;
-				max=v;
-				a(i+1,max,v);
-			}
-			else
-			{
-				max=v;
-				v=h[i+1];
-				a(i+1,max,v);
-			}
-		}
-	}
-	return;
-}
+const int N = 1003;
+int a[N];
 
 int main()
 {
-	int max;
-	int v;
-	cin>>n;
-	for(int i=0;i<n;i++)
-		cin>>h[i];
-	max=h[0];
-	v=h[0];
-	a(0,max,v);
-	cout<<max<<endl;
-	return 0;	
-} 
+	//freopen("input.txt","r",stdin);
+    int n;
+    cin>>n;
+    for(int i=0;i<n;i++)
+    	cin>>a[i];
+    int ans=-1;
+    for(int i=0;i<n;i++)
+    {
+        int low=a[i];
+        for(int j=i;j<n;j++)
+        {
+			if(a[j]<low)
+                low=a[j];
+            int tmp=(j-i+1)*low;
+            if(tmp>ans)
+                ans=tmp;
+        }
+    }
+    cout<<ans<<endl;
+    return 0;
+}
